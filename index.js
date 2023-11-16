7c578d15ad42d1b1c55222bc352b62e857b9e596
Felipe Coimbra <137563995+felipecoimbra123@users.noreply.github.com>
2023-11-16 18:07:10 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+// ------------------------------------------ Coleta e Análise de Dados do Usuário
+
+// let nome
+// let cidade
+
+// console.log(typeof nome, typeof cidade)
+
+// nome = prompt("Qual seu nome")
+// cidade = prompt("Qual sua idade")
+
+// console.log(typeof nome, typeof cidade)
+// console.log(`Olá, meu nome é ${nome} e eu moro em ${cidade}`)
+
+// // ------------------------------------------ Perguntas Sim ou Não
+
+// let perguntaUm = prompt('Você já viajou de avião? ')
+// let perguntaDois = prompt('Você ja comeu batata? ')
+// let perguntaTres = prompt('Você gosta de programar? ')
+
+// console.log(`Você já viajou de avião ${perguntaUm}\n Você já comeu batata? ${perguntaDois}\n Você gosta de programar? ${perguntaTres}`)
+
+// // ------------------------------------------ Troca de Valores entre Variáveis
+
+// let x = 96
+// let y = 69
+// let z = x
+
+// x = y
+// y = z
+
+// console.log(x, y);
+
+// // ------------------------------------------ Crie um programa que peça ao usuário para inserir dois números.
+
+// let numeroUm = Number(prompt("Digite o primeiro número: "))
+// let numeroDois = Number(prompt("Digite o segundo número: "))
+
+// const soma = numeroUm + numeroDois
+// const multiplicação = numeroUm * numeroDois
+
+// console.log(`A soma dos números é: ${soma}`)
+// console.log(`A multiplicação dos números é ${multiplicação}`)
